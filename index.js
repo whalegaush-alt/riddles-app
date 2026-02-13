0e7e04dbf4e5e98a2ea441d6b95b8b9def7a8398
whalegaush-alt <whale.gaush@gmail.com>
2026-02-13 23:19:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const path = require('path');
+const { Telegraf, Markup } = require('telegraf');
+require('dotenv').config();
 
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Раздаем статические файлы из папки frontend
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Команда /start
+bot.start((ctx) => {
+  const webAppUrl = process.env.URL; // Эту ссылку мы вставим позже в Railway
+  ctx.reply('Добро пожаловать! Разгадай наши загадки:', Markup.inlineKeyboard([
+    Markup.button.webApp('Открыть загадки', webAppUrl || 'https://google.com')
+  ]));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+bot.launch();
